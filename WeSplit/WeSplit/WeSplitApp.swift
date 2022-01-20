34278cfa6f1a25dc054b7bc6f2ed07dbaa8b65aa
Young June Park <65703011+pyj9748@users.noreply.github.com>
2022-01-20 20:23:52 +0900

    WeSplit
    SwiftUI App basic structure, Form, Text , TextField, Picker, keyboard , @State , @FocusState

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by young june Park on 2022/01/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
